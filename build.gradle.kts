21c909019d87e90d9ab57ffb9f2c8c4eb89c46be
Scui <kaka128@gmx.de>
2024-01-28 12:02:04 +0100

    Use cio engine for testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,6 @@
 import com.github.jengelman.gradle.plugins.shadow.tasks.ShadowJar
 
-val ktorVersion = "2.3.3"
+val ktorVersion = "2.3.7"
 plugins {
     kotlin("jvm") version "1.9.10"
     kotlin("plugin.serialization") version "1.9.10"
@@ -30,7 +30,7 @@ dependencies {
     implementation("io.ktor:ktor-client-java:$ktorVersion")
     implementation("io.ktor:ktor-server-status-pages-jvm:$ktorVersion")
     implementation("io.ktor:ktor-server-core:$ktorVersion")
-    implementation("io.ktor:ktor-server-jetty:$ktorVersion")
+    implementation("io.ktor:ktor-server-cio:$ktorVersion")
     implementation("io.ktor:ktor-server-content-negotiation-jvm:$ktorVersion")
     implementation("io.ktor:ktor-serialization-kotlinx-json-jvm:$ktorVersion")
     testImplementation(kotlin("test"))
@@ -45,7 +45,7 @@ kotlin {
 }
 
 application {
-    mainClass.set("io.ktor.server.jetty.EngineMain")
+    mainClass.set("io.ktor.server.cio.EngineMain")
 }
 
 tasks.withType<ShadowJar> {
